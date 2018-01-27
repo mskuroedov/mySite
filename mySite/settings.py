36e89ee5b1f5faab2b6d117ac4f886d4beb7e6b0
kuroedov_maxim <Ni7pfpNh>
2018-01-28 01:08:59 +0300

    ..frv.

--- mySite/settings.py
+++ mySite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(__file__)
 PUBLIC_DIR = os.path.join(PROJECT_DIR, 'public')
 STATIC_ROOT = os.path.join(PUBLIC_DIR, 'static')
-STATIC_URL = '/dist/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(PUBLIC_DIR, 'media')
 MEDIA_URL = '/media/'
