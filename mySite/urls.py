595c7b1088c4dee11df6a4126820f0549aae2553
kuroedov_maxim <Ni7pfpNh>
2018-01-28 01:17:33 +0300

    ..frv.

--- mySite/urls.py
+++ mySite/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from core.views import index
+from mySite.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
